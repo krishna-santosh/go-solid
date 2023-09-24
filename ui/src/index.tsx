c404d0f1ba36d586b58ba985d634ff986930f23b
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2023-09-24 15:58:08 +0530

    style: prettier format

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,17 +1,17 @@
 /* @refresh reload */
-import { render } from "solid-js/web";
+import { render } from 'solid-js/web'
 
-import "./index.css";
-import App from "./App";
-import { Router, hashIntegration } from "@solidjs/router";
+import './index.css'
+import App from './App'
+import { Router, hashIntegration } from '@solidjs/router'
 
-const root = document.getElementById("root");
+const root = document.getElementById('root')
 
 render(
-  () => (
-    <Router source={hashIntegration()}>
-      <App />
-    </Router>
-  ),
-  root!
-);
+	() => (
+		<Router source={hashIntegration()}>
+			<App />
+		</Router>
+	),
+	root!,
+)
