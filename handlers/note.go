9680ae485a5fb0d60025f3ee087d7aa00d69592c
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2023-10-04 10:45:39 +0530

    fix: remove redundant { } and white spaces.

--- handlers/note.go
+++ handlers/note.go
@@ -18,7 +18,7 @@ func CreateNote(c *fiber.Ctx) error {
 	var note models.Note
 
 	if err := c.BodyParser(&note); err != nil {
-		log.Printf("Couldn't parse the response body. \n{%s}", err.Error())
+		log.Printf("Couldn't parse the response body. \n%s", err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err.Error()})
 	}
 
@@ -28,11 +28,11 @@ func CreateNote(c *fiber.Ctx) error {
 	}
 
 	if err := db.Create(&note).Error; err != nil {
-		log.Printf("Couldn't create note \n{%s}\n", err.Error())
+		log.Printf("Couldn't create note \n%s\n", err.Error())
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Couldn't create note", "data": err.Error()})
 	}
 
-	log.Printf("Created Note {%d} Successfully.\n", note.ID)
+	log.Printf("Created Note %d Successfully.\n", note.ID)
 
 	return c.Status(201).JSON(fiber.Map{"status": "success", "message": "Created note successfully", "data": note})
 }
@@ -44,12 +44,12 @@ func GetNote(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
 	if err != nil {
-		log.Printf("Couldn't parse the id from params\n {%s}\n", err.Error())
+		log.Printf("Couldn't parse the id from params\n%s\n", err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Couldn't parse the id", "data": err.Error()})
 	}
 
 	if err = db.First(&note, id).Error; err != nil {
-		log.Printf("Couldn't get note \n{%s}\n", err.Error())
+		log.Printf("Couldn't get note \n%s\n", err.Error())
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Couldn't get note", "data": err.Error()})
 	}
 
@@ -61,7 +61,7 @@ func GetAllNotes(c *fiber.Ctx) error {
 	var notes []models.Note
 
 	if err := db.Find(&notes).Error; err != nil {
-		log.Printf("Couldn't get notes \n{%s}\n", err.Error())
+		log.Printf("Couldn't get notes \n%s\n", err.Error())
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Couldn't get notes", "data": err.Error()})
 	}
 
@@ -75,27 +75,27 @@ func UpdateNote(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
 	if err != nil {
-		log.Printf("Couldn't parse the id from params\n {%s}\n", err.Error())
+		log.Printf("Couldn't parse the id from params\n%s\n", err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Couldn't parse the id", "data": err.Error()})
 	}
 
 	if err := db.First(&note, id).Error; err != nil {
-		log.Printf("Couldn't get the note with id %d\n %s", id, err.Error())
+		log.Printf("Couldn't get the note with id %d\n%s", id, err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Couldn't find the note with id " + fmt.Sprint(id), "data": err.Error()})
 	}
 
 	if err := c.BodyParser(&note); err != nil {
-		log.Printf("Couldn't parse the response body. \n{%s}", err.Error())
+		log.Printf("Couldn't parse the response body. \n%s", err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err.Error()})
 	}
 
 	if err := validate.Struct(&note); err != nil {
-		log.Printf("Validation Error %s", err.Error())
+		log.Printf("Validation Error: %s", err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err.Error()})
 	}
 
 	if err := db.Save(&note).Error; err != nil {
-		log.Printf("Couldn't update the note.\n{%s}\n", err.Error())
+		log.Printf("Couldn't update the note.\n%s\n", err.Error())
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Couldn't update the note", "data": err.Error()})
 	}
 
@@ -108,15 +108,15 @@ func DeleteNote(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
 	if err != nil {
-		log.Printf("Couldn't parse the id from params\n {%s}\n", err.Error())
+		log.Printf("Couldn't parse the id from params\n%s\n", err.Error())
 		return c.Status(400).JSON(fiber.Map{"status": "error", "message": "Couldn't parse the id", "data": err.Error()})
 	}
 
-	log.Printf("Deleting Note {%d}...\n", id)
+	log.Printf("Deleting Note %d...\n", id)
 	if err = db.Unscoped().Delete(&models.Note{}, id).Error; err != nil {
-		log.Printf("Couldn't delete the note with %d from the database.\n {%s}\n", id, err)
+		log.Printf("Couldn't delete the note with %d from the database.\n%s\n", id, err)
 	}
-	log.Printf("Deleted Note {%d} Successfully.\n", id)
+	log.Printf("Deleted Note %d Successfully.\n", id)
 
 	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "Deleted note successfully"})
 }
